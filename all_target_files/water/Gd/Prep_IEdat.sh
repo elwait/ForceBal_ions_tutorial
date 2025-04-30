5d40adfe4a48782fb263b0a4b927aeaec0287d4f
Elizabeth E. Wait <51386555+elwait@users.noreply.github.com>
2025-04-30 12:33:17 -0500

    Add files via upload

---
+++ all_target_files/water/Gd/Prep_IEdat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ion="Gd"                                # atomic symbol for current ion
+echo ${ion}
+ligand="water"                          # current ligand (water, acetate, water)
+echo ${ligand}
+
+touch IE.dat
+while IFS= read -r line; do
+# for each distance listed in the file
+    echo ${line}
+    name=$(echo ${line} | awk '{ print $1 }' | tr -d ' ' )
+    energy=$(echo ${line} | awk '{ print $3 }' | tr -d ' ' )
+    weight=$(echo ${line} | awk '{ print $4 }' | tr -d ' ' )
+    xyz="${ion}_${ligand}_${name}_t.xyz"
+    echo "${xyz}   ${energy}    ${weight}" >> IE.dat
+done < ${ion}_${ligand}_name_letter_energy_weight_dist.txt
+
